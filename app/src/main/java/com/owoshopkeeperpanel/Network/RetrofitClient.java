0c3b593bf1c5041095f9f9035786e52f8a1a457f
Amimul <1805056@ugrad.cse.buet.ac.bd>
2020-09-13 14:07:57 +0600

    Server changed

--- app/src/main/java/com/owoshopkeeperpanel/Network/RetrofitClient.java
+++ app/src/main/java/com/owoshopkeeperpanel/Network/RetrofitClient.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-    private static final String BASE_URL = "http://learningabc.tech/OwoDokan/public/";
+    private static final String BASE_URL = "http://learningabc.tech/";
     private static RetrofitClient mInstance;
     private Retrofit retrofit;
 
