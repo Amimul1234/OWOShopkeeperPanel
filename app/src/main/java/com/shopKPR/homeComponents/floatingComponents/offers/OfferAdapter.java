7d913e6002cefafdb8ac4864506e77ad73d91f96
Amimul <amimulahsan7@gmail.com>
2021-04-24 21:57:09 +0600

    Correcting login.

--- app/src/main/java/com/shopKPR/homeComponents/floatingComponents/offers/OfferAdapter.java
+++ app/src/main/java/com/shopKPR/homeComponents/floatingComponents/offers/OfferAdapter.java
@@ -1,16 +1,13 @@
 package com.shopKPR.homeComponents.floatingComponents.offers;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Paint;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
